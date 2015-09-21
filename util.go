8c081994c91e2b7faa7154a920332aa9a5ffcb73
Marek Dolgos <marek@lytics.io>
2015-09-21 14:36:16 -0700

    Adding a seeded rand utility.

---
+++ util.go
@@ -0,0 +1,26 @@
+package grid
+
+import (
+	"encoding/binary"
+	"hash/fnv"
+	"math/rand"
+	"time"
+)
+
+// NewSeededRand creates a *math/rand.Rand with a seed generated
+// by NewSeed.
+func NewSeededRand() *rand.Rand {
+	return rand.New(rand.NewSource(NewSeed()))
+}
+
+// NewSeed generate a seed by running time.Now().UnixNano()
+// through 10,000 rounds of an FNV hash.
+func NewSeed() int64 {
+	h := fnv.New64()
+	for i := 0; i < 10000; i++ {
+		b := make([]byte, binary.MaxVarintLen64)
+		binary.PutVarint(b, time.Now().UnixNano())
+		h.Write(b)
+	}
+	return int64(h.Sum64())
+}
