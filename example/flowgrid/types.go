144ba222614af8e83d823a4b058a5658e92f9806
Marek Dolgos <marek@lytics.io>
2015-09-24 09:50:07 -0700

    Moved dfa states into types.go

--- example/flowgrid/types.go
+++ example/flowgrid/types.go
@@ -5,7 +5,28 @@ import (
 	"log"
 	"time"
 
+	"github.com/lytics/dfa"
 	"github.com/lytics/grid"
+)
+
+var (
+	// States
+	Starting    = dfa.State("starting")
+	Running     = dfa.State("running")
+	Resending   = dfa.State("resending")
+	Finishing   = dfa.State("finishing")
+	Exiting     = dfa.State("exiting")
+	Terminating = dfa.State("terminating")
+	// Letters
+	Failure            = dfa.Letter("failure")
+	SendFailure        = dfa.Letter("send-failure")
+	SendSuccess        = dfa.Letter("send-success")
+	FetchStateFailure  = dfa.Letter("fetch-state-failure")
+	StoreStateFailure  = dfa.Letter("store-state-failure")
+	EverybodyStarted   = dfa.Letter("everybody-started")
+	EverybodyFinished  = dfa.Letter("everybody-finished")
+	IndividualFinished = dfa.Letter("individual-finished")
+	Exit               = dfa.Letter("exit")
 )
 
 func init() {
