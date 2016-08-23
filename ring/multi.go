2db2ce6c5f3341faec56dc614016ab00a7f89d69
Marek Dolgos <marek@lytics.io>
2016-08-23 00:26:23 -0700

    New method on multi-ring.

--- ring/multi.go
+++ ring/multi.go
@@ -11,26 +11,33 @@ import (
 type MultiRing interface {
 	Rings() []Ring
 	ByHashedString(key string) Ring
+	ByReservedHashedString(key string) Ring
 }
 
 type multi struct {
-	r     int
-	dice  *rand.Rand
-	rings []Ring
+	totalRings    int
+	reservedRings int
+	dice          *rand.Rand
+	rings         []Ring
 }
 
-func NewMultiRing(name string, n, r int) MultiRing {
-	rings := make([]Ring, r)
-	for i := 0; i < r; i++ {
-		r := New(fmt.Sprintf("%v-%v", name, i), n)
+func NewMultiRing(name string, members, totalRings, reservedRings int) MultiRing {
+	rings := make([]Ring, totalRings)
+	for i := 0; i < totalRings; i++ {
+		r := New(fmt.Sprintf("%v-%v", name, i), members)
 		r.(*ring).actortype = name
 		rings[i] = r
 	}
-	return &multi{r: r, dice: grid.NewSeededRand(), rings: rings}
+	return &multi{
+		totalRings:    totalRings,
+		reservedRings: reservedRings,
+		dice:          grid.NewSeededRand(),
+		rings:         rings,
+	}
 }
 
 func (m *multi) Rings() []Ring {
-	rings := make([]Ring, m.r)
+	rings := make([]Ring, m.totalRings)
 	copy(rings, m.rings)
 	return rings
 }
@@ -38,6 +45,13 @@ func (m *multi) Rings() []Ring {
 func (m *multi) ByHashedString(key string) Ring {
 	h := fnv.New64()
 	h.Write([]byte(key))
-	i := h.Sum64() % uint64(m.r)
+	i := h.Sum64() % uint64(m.totalRings-m.reservedRings)
+	return m.rings[i]
+}
+
+func (m *multi) ByReservedHashedString(key string) Ring {
+	h := fnv.New64()
+	h.Write([]byte(key))
+	i := (m.totalRings - m.reservedRings) + int(h.Sum64()%uint64(m.reservedRings))
 	return m.rings[i]
 }
