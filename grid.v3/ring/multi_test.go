f72cfda5c1cda3d38103635f6ae46d329596e784
mdmarek <marek@lytics.io>
2017-01-04 06:26:17 -0800

    Fix ring.

--- grid.v3/ring/multi_test.go
+++ grid.v3/ring/multi_test.go
@@ -9,21 +9,21 @@ func TestMultiRing(t *testing.T) {
 	)
 
 	expected := make(map[string]bool)
-	expected["testing-reader-0-0"] = true
-	expected["testing-reader-0-1"] = true
-	expected["testing-reader-1-0"] = true
-	expected["testing-reader-1-1"] = true
+	expected["reader-0-0"] = true
+	expected["reader-0-1"] = true
+	expected["reader-1-0"] = true
+	expected["reader-1-1"] = true
 
-	m := NewMultiRing(namespace, name, 2, 2, 0)
+	m := NewMultiRing(name, 2, 2, 0)
 	for _, r := range m.Rings() {
 		for _, def := range r.ActorDefs() {
-			if !expected[def.ID()] {
-				t.Fatalf("expected to find: %v", def.ID())
+			if !expected[def.Name] {
+				t.Fatalf("expected to find: %v", def.Name)
 			}
 			if def.Type != "reader" {
 				t.Fatalf("expected actor type to be 'reader'")
 			}
-			delete(expected, def.ID())
+			delete(expected, def.Name)
 		}
 	}
 	if len(expected) != 0 {
@@ -37,7 +37,7 @@ func TestMultiRingByHashedString(t *testing.T) {
 		namespace = "namespace"
 	)
 
-	m := NewMultiRing(namespace, name, 2, 20, 0)
+	m := NewMultiRing(name, 2, 20, 0)
 
 	if r := m.ByHashedString("group-0"); r.(*ring).name != "reader-17" {
 		t.Fatalf("expected '%v' to hash to ring '%v'", "group-0", "")
