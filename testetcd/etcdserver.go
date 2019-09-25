4eb76c90183fb1fda5f94dd5a2a44feed0b5cfd1
Graham Forest <graham.forest@lytics.com>
2019-09-25 09:37:56 -0700

    Remove accidentally added left in timeout

--- testetcd/etcdserver.go
+++ testetcd/etcdserver.go
@@ -2,15 +2,13 @@ package testetcd
 
 import (
 	"testing"
-	"time"
 
 	"go.etcd.io/etcd/clientv3"
 )
 
 func StartAndConnect(t testing.TB) *clientv3.Client {
 	cfg := clientv3.Config{
-		Endpoints:   []string{"localhost:2379"},
-		DialTimeout: 500 * time.Millisecond,
+		Endpoints: []string{"localhost:2379"},
 	}
 	etcd, err := clientv3.New(cfg)
 	if err != nil {
